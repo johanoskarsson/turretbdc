154c9659e9ef877902b2b1e00dc10ba93e3fbaf4
Johan Oskarsson <johan@oskarsson.nu>
2017-01-18 19:50:29 -0800

    Add a small circle to show when to bump windage (due to spin drift) in your calculations. Also add readme to describe how to generate the input

--- README.md
+++ README.md
@@ -19,6 +19,17 @@ This is a Ruby program so you need Ruby installed. Below is assuming MacOS and H
 
 See example_input.csv for an example of what kind of data the program expects.
 
+# Generating input csv file
+
+1. Click Edit -> Units -> Input -> Metric
+2. Pick your bullet from the database. Use Applied Ballistics Custom Drag Curves if available.
+3. Enter Muzzle velocity, Zerorange, sight height and twist rate as appropriate
+4. Set "Avg. Wind Speed" to 0. Leave spin drift on.
+5. Set temperature, pressure and humidity as you have measure or expect it to be when shooting.
+6. Set heading to 0.
+7. You'll get one mark per entry in the range card, so set increment and max distance to whatever you'd like printed.
+8. File -> Export range card
+
 # Known issues
 
 This has only been tested with mils and the metric system.
--- bdc_generator.rb
+++ bdc_generator.rb
@@ -9,23 +9,45 @@ class BDCGenerator
 
     Prawn::Document.generate(file, page_size: page_size, margin: margin) do
       bounding_box([0, turret.height_mm.mm], :width => turret.circumference_mm.mm, :height => turret.height_mm.mm) do
+        line_width(0.2.mm)
         stroke_bounds
 
+        # used to figure out when we "tick" over to the next windage number
+        prev_windage_clicks = 0
+
         ballistics.distances.each do |distance|
-          even_hundred = distance % 100 == 0
           elevation_mils = ballistics.elevation_mils(distance)
           elevation_pos = turret.mm_per_mil * elevation_mils
+
           # to make it easier to spot the 100 marks, reduce all the others in size
-          line_height = even_hundred ? (turret.height_mm.mm / 2) : (turret.height_mm.mm / 3)
+          line_height = if distance % 100 == 0 then
+            turret.height_mm.mm / 2
+          elsif distance % 50 == 0
+            turret.height_mm.mm / 3
+          else
+            turret.height_mm.mm / 6
+          end
 
           # the vertical lines that mark the distances
           stroke { line [elevation_pos.mm, 0], [elevation_pos.mm, line_height] }
 
+          # marking windage (spin drift) so we can look on the elevation turret
+          # what we need to add to our windage calculations
+          windage_clicks = (ballistics.windage_mils(distance) * 10).round
+          if windage_clicks > prev_windage_clicks
+            circle_y = turret.height_mm.mm / 2
+            circle_x = elevation_pos.mm - (0.5.mm / 2)
+            circle([circle_x, circle_y], 0.5.mm)
+            fill()
+          end
+          prev_windage_clicks = windage_clicks
+
           # only print numbers at even 100 marks
-          unless even_hundred
+          unless distance % 100 == 0
             next
           end
 
+          # figure out where to write the distance labels
           distance_hundreds = (distance / 100).to_s
           text_y = (turret.height_mm.mm - (turret.height_mm.mm / 3))
           text_width = width_of(distance_hundreds, :size => 10)
